21090d78955d14ae1b43c617847712c3de120765
YakymivDanylo <dr.yakimiv@gmail.com>
2024-09-16 15:12:15 +0300

    Lab4 ex2 done

--- Laba4/Laba4/src/Ex2.java
+++ Laba4/Laba4/src/Ex2.java
@@ -1,5 +1,19 @@
-public class Ex2 {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+public class Ex2 {
+    public static List<Integer> getIntegers(List<Optional<Integer>> optionals){
+        return optionals.stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
 
     public static void main(String[] args) {
+        List<Optional<Integer>> optionals = List.of(Optional.of(1),Optional.empty(),Optional.of(2),Optional.of(3),Optional.empty());
+        List<Integer> integers = getIntegers(optionals);
+        System.out.println(integers);
+
+    }
 }
